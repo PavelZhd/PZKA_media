d069543e3456f040ce20f8a88e292873122c4174
PavelZhd <hilvon1984@gmail.com>
2025-07-17 21:58:32 +0300

    Update popup lights function to optimize interaction with VVA version

--- lua/client/PZKA_popupLights.lua
+++ lua/client/PZKA_popupLights.lua
@@ -1,17 +1,27 @@
-local Cached_ISVehicleMenu_onToggleHeadlights = ISVehicleMenu.onToggleHeadlights
-function ISVehicleMenu.onToggleHeadlights(playerObj)
-    Cached_ISVehicleMenu_onToggleHeadlights(playerObj)
-	local vehicle = playerObj:getVehicle()
-	if not vehicle then return end
-	print("PZKA: (C) LightsOn local handler")
-    local part = vehicle:getPartById("PZKA_LightsHost")
-    if not part then return end
-	if vehicle:getHeadlightsOn() then
-        vehicle:playPartAnim(part, "Close")
-        part:getDoor():setOpen(false)
-    	vehicle:transmitPartDoor(part)
-    else
-        vehicle:playPartAnim(part, "Open")
-        part:getDoor():setOpen(true)
-    	vehicle:transmitPartDoor(part)    end
+require "Vehicles/ISUI/ISVehicleMenu"
+vvaLightsFunctionOverride = vvaLightsFunctionOverride or false
+if not (vvaLightsFunctionOverride > 1) then
+	vvaLightsFunctionOverride = 1
+	local Cached_ISVehicleMenu_onToggleHeadlights = ISVehicleMenu.onToggleHeadlights
+	function ISVehicleMenu.onToggleHeadlights(playerObj)
+		Cached_ISVehicleMenu_onToggleHeadlights(playerObj)
+		local vehicle = playerObj:getVehicle()
+		if not vehicle then return end
+		print("PZKA: (C) LightsOn local handler")
+		local part = vehicle:getPartById("PZKA_LightsHost")
+		if not part then return end
+		if vehicle:getHeadlightsOn() then
+			vehicle:playPartAnim(part, "Close")
+			if part:getDoor() then
+                part:getDoor():setOpen(false)
+			    vehicle:transmitPartDoor(part)
+            end
+		else
+			vehicle:playPartAnim(part, "Open")
+			if part:getDoor() then
+                part:getDoor():setOpen(true)
+			    vehicle:transmitPartDoor(part)
+            end
+		end
+	end
 end
