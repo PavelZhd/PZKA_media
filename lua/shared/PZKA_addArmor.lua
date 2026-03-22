a5a8cafab98f26970f0ac494675ee07816c1dee4
PavelZhd <hilvon1984@gmail.com>
2026-03-22 12:36:14 +0300

    Fixed Cerise93/Triumph bugs. Added SVU armor template.

--- lua/shared/PZKA_addArmor.lua
+++ lua/shared/PZKA_addArmor.lua
@@ -11,5 +11,14 @@ if PZKA.CheckModEnabled("StandardizedVehicleUpgrades3V") then
 			handler.plus({armor})
 		end
 	end
-	--LinkAnimationsToArmor("pzkChevalierCerise93", 				"PZKA_SVU_Armor_Cerise93")
+	LinkAnimationsToArmor("pzkChevalierCerise93", 				"PZKA_SVU_Armor_Cerise93")
+	LinkAnimationsToArmor("pzkChevalierCerise93Service", 		"PZKA_SVU_Armor_Cerise93_nr")
+	LinkAnimationsToArmor("pzkChevalierCerise93Taxi", 		    "PZKA_SVU_Armor_Cerise93_nr")
+	LinkAnimationsToArmor("pzkChevalierCerise93PoliceK9", 		"PZKA_SVU_Armor_Cerise93_nr")
+
+    LinkAnimationsToArmor("pzkFranklinTriumph", 				"PZKA_SVU_Armor_Cerise93")
+	LinkAnimationsToArmor("pzkFranklinTriumphService", 		    "PZKA_SVU_Armor_Cerise93_nr")
+	LinkAnimationsToArmor("pzkFranklinTriumphTaxi", 		    "PZKA_SVU_Armor_Cerise93_nr")
+	LinkAnimationsToArmor("pzkChevalierTriumph3PoliceK9", 		"PZKA_SVU_Armor_Cerise93_nr")
+
 end
